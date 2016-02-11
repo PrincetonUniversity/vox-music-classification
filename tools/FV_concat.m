24a55e6aa2f17cfba8dfb2d5b37d2539cedf1f59
Vladimir Feinberg <vyf@princeton.edu>
2016-02-10 23:28:19 -0500

    added matlab tooling (untested since I don't have matlab)

---
+++ tools/FV_concat.m
@@ -0,0 +1,25 @@
+function FullFV = fv_all_mfc()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FV Concatenating
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Change this to the directory containing your data folder
+% add tools path
+run('vlfeat/toolbox/vl_setup')
+[DAT, LB, FNS] = loadAll('..');
+
+%extract the MFCC
+mfcc = cell(1,length(DAT));
+
+for i = 1:length(DAT)
+    mfcc{i} = DAT{i}.mfc;
+end
+
+%create the structure used as input into the demo_fv
+GENDATA.data = mfcc;
+GENDATA.class = LB;
+GENDATA.classnames = {'Blues', 'Classical', 'Country', 'Disco', 'Hiphop',...
+	'Jazz', 'Metal', 'Pop', 'Reggae', 'Rock'};
+%run fisher vector
+FV = demo_fv(GENDATA, 3, 3);
+save('../data/FV.mat','FV');
+save('../data/LB.mat','LB');
